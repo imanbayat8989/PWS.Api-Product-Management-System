a979f9da8cc943fb8bb2c3b2c511721792d56c46
iman bayat mokhtari <105519831+imanbayat8989@users.noreply.github.com>
2023-08-19 01:14:45 +0430

    Add project files.

---
+++ PWS.Api/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PWS.Api.Data;
+using PWS.Api.Models;
+
+namespace PWS.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly PWSDbContext _PwsDbContext;
+        public ProductsController(PWSDbContext pWSDbContext)
+        {
+            this ._PwsDbContext = pWSDbContext;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            var products = await _PwsDbContext.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddProduct([FromBody]Product product)
+        {
+            product.Id = Guid.NewGuid();
+
+            _PwsDbContext.Products.AddAsync(product);
+            await _PwsDbContext.SaveChangesAsync();
+
+            return Ok(product);
+        }
+    }
+}
---
+++ PWS.Api/Data/PWSDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PWS.Api.Models;
+
+namespace PWS.Api.Data
+{
+    public class PWSDbContext : DbContext
+    {
+        public PWSDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
